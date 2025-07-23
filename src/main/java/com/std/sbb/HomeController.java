05bcc285bd15a7d7a3900c59986a3df109d5b2e5
jiseon723 <dighxxx12@gmail.com>
2025-07-23 16:28:40 +0900

    사칙연산(+, -, *, /) 메서드, 게터 세터 생성

--- src/main/java/com/std/sbb/HomeController.java
+++ src/main/java/com/std/sbb/HomeController.java
@@ -1,8 +1,16 @@
 package com.std.sbb;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // @Controller의 의미 :  개발자가 스프링부트에게 말한다. >> 아래에 있는 HomeController는 컨트롤러다.
 @Controller
@@ -37,4 +45,72 @@ public class HomeController {
         i++;
         return i;
     }
+
+    @GetMapping("/home/plus")
+    @ResponseBody
+    public int plus(@RequestParam(value = "a", defaultValue = "0") int a,
+                    @RequestParam(value = "b", defaultValue = "0") int b) {
+        return a + b;
+    }
+
+    @GetMapping("/home/minus")
+    @ResponseBody
+    public int minus(@RequestParam(value = "a", defaultValue = "0") int a,
+                     @RequestParam(value = "b", defaultValue = "0") int b) {
+        return a - b;
+    }
+
+    @GetMapping("/home/multifly")
+    @ResponseBody
+    public int multifly(@RequestParam(value = "a", defaultValue = "0") int a,
+                        @RequestParam(value = "b", defaultValue = "0") int b) {
+        return a * b;
+    }
+
+    @GetMapping("/home/divid")
+    @ResponseBody
+    public int divid(@RequestParam(value = "a", defaultValue = "0") int a,
+                     @RequestParam(value = "b", defaultValue = "0") int b) {
+        return a / b;
+    }
+
+    @GetMapping("/home/calc")
+    @ResponseBody
+    public int calc(@RequestParam(value = "a", defaultValue = "0") int a,
+                    @RequestParam(value = "b", defaultValue = "0") int b,
+                    @RequestParam(value = "c") String c) {
+        if (c.equals("pl")) return a + b;
+        if (c.equals("mi")) return a - b;
+        if (c.equals("mu")) return a * b;
+        if (c.equals("di")) return a / b;
+        return a;
+    }
+
+    @GetMapping("/home/returnCar")
+    @ResponseBody
+    public Car showReturnCar () {
+        Car car = new Car();
+        car.setId(2);
+        car.setSpeed(200);
+        car.setName("랭글러");
+        car.setIds(new ArrayList(){{
+            add(1);
+            add(2);
+            add(3);
+        }});
+        return car;
+    }
+}
+
+// 전체 속성을 포함하는 생성자 함수 생성
+//게터 세터 생성
+@AllArgsConstructor
+@NoArgsConstructor //값이 없을 때의 처리
+@Getter
+@Setter
+class Car {
+    private int id;
+    private int speed;
+    private String name;
+    private List<Integer> ids;
 }
